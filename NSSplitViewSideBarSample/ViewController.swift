edec1ec84c36ff978874301802f8e78b2e74fc6b
vong-e <sinkim123321@gmail.com>
2023-03-01 03:26:13 +0900

    Initial Commit

---
+++ NSSplitViewSideBarSample/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  NSSplitViewSideBarSample
+//
+//  Created by vongvorovongvong on 2023/03/01.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
