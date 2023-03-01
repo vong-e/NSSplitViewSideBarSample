aa1f855148c7d5abe09fed0989b2e9250f55dfcc
vong-e <sinkim123321@gmail.com>
2023-03-01 23:33:25 +0900

    Feat: Dock에서 앱 아이콘 클릭 시 MainWindowController visible여부에 따른 처리
    Dock에서 앱 아이콘 클릭 시 MainWindowController가 close버튼 등으로 보이지 않는 상태일 때 show하는 기능 추가

--- NSSplitViewSideBarSample/AppDelegate.swift
+++ NSSplitViewSideBarSample/AppDelegate.swift
@@ -21,10 +21,19 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to tear down your application
     }
 
+    func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+        if flag {
+            print("has visible window")
+            return true
+        }
+        
+        print("has not visible window")
+        mainWindowController.showWindow(nil)
+        return false
+    }
+    
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
         return true
     }
-
-
 }
 
