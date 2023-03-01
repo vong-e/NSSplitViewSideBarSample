45a113fd08231969e4281524bd986a548ec6e5c5
vong-e <sinkim123321@gmail.com>
2023-03-01 23:14:15 +0900

    Feat: Add MainWindowController

--- NSSplitViewSideBarSample/AppDelegate.swift
+++ NSSplitViewSideBarSample/AppDelegate.swift
@@ -10,11 +10,11 @@ import Cocoa
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    
+    lazy var mainWindowController = MainWindowController()
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        mainWindowController.showWindow(nil)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
---
+++ NSSplitViewSideBarSample/WindowController/MainWindowController.swift
@@ -0,0 +1,20 @@
+//
+//  MainWindowController.swift
+//  NSSplitViewSideBarSample
+//
+//  Created by vongvorovongvong on 2023/03/01.
+//
+
+import Cocoa
+
+final class MainWindowController: NSWindowController {
+
+    convenience init() {
+        self.init(windowNibName: "MainWindowController")
+    }
+    
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    }
+    
+}
