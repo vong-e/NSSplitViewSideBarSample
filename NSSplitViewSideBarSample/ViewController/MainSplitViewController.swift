0a536ea8d10b67e20db005ff212d1b4495079718
vong-e <sinkim123321@gmail.com>
2023-03-02 04:04:59 +0900

    Feat: MainSplitVC에 SideBarVC 추가

--- NSSplitViewSideBarSample/ViewController/MainSplitViewController.swift
+++ NSSplitViewSideBarSample/ViewController/MainSplitViewController.swift
@@ -8,11 +8,37 @@
 import Cocoa
 
 final class MainSplitViewController: NSSplitViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.view.wantsLayer = true
-        self.view.layer?.backgroundColor = NSColor.red.cgColor
+    
+    private let webViewContentVC = WebViewContentViewController()
+    private let sideBarVC = SideBarViewController()
+    
+    // MARK: - Initialize
+    init() {
+        super.init(nibName: nil, bundle: nil)
     }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+    }
+    
+    // MARK: - Configure
+    private func configure() {
+        let webViewSplitViewItem = webViewContentVC.toSplitViewItem()
+        self.addSplitViewItem(webViewSplitViewItem)
+        
+        let sideBarSplitViewItem = sideBarVC.toSplitViewItem()
+        self.addSplitViewItem(sideBarSplitViewItem)
+        sideBarSplitViewItem.minimumThickness = 40
+    }
+    
+    /// Divider Size
+    override func splitView(_ splitView: NSSplitView, effectiveRect proposedEffectiveRect: NSRect, forDrawnRect drawnRect: NSRect, ofDividerAt dividerIndex: Int) -> NSRect {
+        return NSRect.zero
+    }
 }
