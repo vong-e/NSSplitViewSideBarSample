edef372a6c1a6ee4f9c031baf1709dd92735f0d2
vong-e <sinkim123321@gmail.com>
2023-03-05 03:03:04 +0900

    Feat: SideBarApplicationMockData 추가

---
+++ NSSplitViewSideBarSample/Mock/SideBarApplicationMockData.swift
@@ -0,0 +1,58 @@
+//
+//  SideBarApplication.swift
+//  NSSplitViewSideBarSample
+//
+//  Created by vongvorovongvong on 2023/03/04.
+//
+
+import AppKit
+
+enum SideBarApplicationMockData: Int, CaseIterable {
+    case translate = 0
+    case webtoon
+    case memo
+    case timer
+    
+    var id: Int {
+        return self.rawValue
+    }
+    
+    var backgroundColor: NSColor {
+        switch self {
+        case .translate:
+            return .blue
+        case .webtoon:
+            return .green
+        case .memo:
+            return .yellow
+        case .timer:
+            return .gray
+        }
+    }
+    
+    var title: String {
+        switch self {
+        case .translate:
+            return "번역"
+        case .webtoon:
+            return "웹툰"
+        case .memo:
+            return "메모"
+        case .timer:
+            return "타이머"
+        }
+    }
+    
+    var logo: NSImage {
+        switch self {
+        case .translate:
+            return .getSideBarAppLogo(image: .translate)
+        case .webtoon:
+            return .getSideBarAppLogo(image: .webtoon)
+        case .memo:
+            return .getSideBarAppLogo(image: .memo)
+        case .timer:
+            return .getSideBarAppLogo(image: .timer)
+        }
+    }
+}
